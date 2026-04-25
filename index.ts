7216ef07d9a46a745b798311ebf2ea9d64f46fa1
Lorgborg <rr0villacater@gmail.com>
2026-04-25 10:19:57 +0800

    added api endpoint /speedwatches/get/matchup

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@
 import express from "express";
 const app = express()
 const port = 3000
-import { connect, disconnect } from 'mongoose';
+import { connect, disconnect, FilterQuery } from 'mongoose';
 
 // riot related imports and .env import
 import "dotenv/config"
@@ -18,6 +18,9 @@ import { summonerUserModel } from "./utils/schemas/summonerUsers"
 import bulkNoteUpdate from "./utils/functions/bulkNoteUpdate";
 import getOpponent from "./utils/functions/getOpponent";
 import getLeagueKey from "./utils/getLeagueKey";
+import { NoteModel } from "./utils/schemas/notes";
+
+app.use(express.json())
 
 // save all necessary info to save on api calls needed when querying data
 app.get("/speedwatches/match/check", async (req, res) => {
@@ -78,13 +81,49 @@ app.get("/speedwatches/match/check", async (req, res) => {
 app.get("/speedwatches/get/users", async (req, res) => {
     try {
         await connect(getLeagueKey())
-    } catch {
-        throw Error("error in /get/user")
+        const username = req.body["username"]
+        console.log(username)
+        
+    } catch (e) {
+        console.log(`error at /get/users \n${e}`)
     } finally {
         disconnect()
     }
 })
 
+app.get("/speedwatches/get/matchup", async (req, res) => {
+    try {
+        await connect(getLeagueKey())
+        const query: FilterQuery<typeof NoteModel> = {
+
+        }
+        if(req.body["username"] != null){
+            const userDbQuery = await summonerUserModel.findOne(
+                { user: req.body["username"] },
+                { puuid: 1 }
+            ).lean()
+            if(userDbQuery == null){
+                console.log("user not found in database")
+                return
+            }
+            query.puuid = userDbQuery.puuid
+        }
+        
+        if(req.body["championPlayed"] != null) query.championPlayed = req.body["championPlayed"]
+        if(req.body["championFighting"] != null) query.championFighting = req.body["championFighting"]
+
+        console.log(`using query: ${query}`)
+        const find = await NoteModel.find(
+            query
+        ).lean()
+
+        res.send(find)
+    } catch (e) {
+        res.send(`error at /get/matchup \n${e}`)
+    } finally {
+        disconnect()
+    }
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
